05ec15bed57ab16809fb9465f377e753079ef202
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-11-14 13:18:25 +0100

    Bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
+# Defines the root path route ("/")
+# root "posts#index"
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "main#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "main#index"
 end
