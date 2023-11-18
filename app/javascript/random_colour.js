ed08ea01824df117736a12f6912b416606c43409
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-11-18 01:03:29 +0100

    Random colour in textfield by clicking on button by JS corrected

--- app/javascript/random_colour.js
+++ app/javascript/random_colour.js
@@ -1,5 +1,5 @@
 // Random colours from given array with those
 function randomColour(a_colours) {
     let randomColor =  a_colours[Math.floor(Math.random() * a_colours.length)]
-    document.getElementById("survey_fav_color").innerHTML = randomColor;
+    document.getElementById("survey_fav_color").value = randomColor;
 }
